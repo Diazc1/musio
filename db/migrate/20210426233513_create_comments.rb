66839af67ee13a0c50238c2b8a4dd1d39016b5e2
Cathy Diaz <Cathydiaz14@gmail.com>
2021-04-26 18:58:00 -0700

    add content column and foreign keys to comments table

--- db/migrate/20210426233513_create_comments.rb
+++ db/migrate/20210426233513_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
+      t.text :content
+      t.belongs_to :user
+      t.belongs_to :song
 
       t.timestamps
     end
