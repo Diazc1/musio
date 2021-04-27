f1bb7407638a625079f8c45b6481cbc801125509
Cathy Diaz <Cathydiaz14@gmail.com>
2021-04-27 13:38:29 -0700

    create signup, login/logout routes separately from the ones resource gives

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   resources :comments
-  resources :songs
+  resources :songs, except: [:new]
   resources :users
+
+  get '/signup', to: 'users#new'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
