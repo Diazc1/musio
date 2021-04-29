bc23a7318f7a39dc015ac1577ce4bce35a6a3904
Cathy Diaz <Cathydiaz14@gmail.com>
2021-04-28 17:20:26 -0700

    create user authentication- signup, login, logout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
   resources :comments
   resources :songs
-  resources :users, except: [:new]
+  resources :users, only: [:new, :create, :show]
 
   get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 
-  get '/login', to: 'sessions#new'
+  get '/login', to: 'sessions#login'
   post '/login', to: 'sessions#create'
 
   delete '/logout', to: 'sessions#destroy'
+
+
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
