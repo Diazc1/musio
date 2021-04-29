61364e1c828865678a837850341f31f8a1001a75
Cathy Diaz <Cathydiaz14@gmail.com>
2021-04-28 17:13:46 -0700

    create sessions controller to sign users in. In the create method, find user by their username and save it to an instance variable. If that user and their password match, sign that user in, otherwise render the log in page again.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,24 @@
 class SessionsController < ApplicationController
     #only use for signing a user in
+    def login 
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_by_username(params[:username])
+
+        if @user && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            render :login
+        end
+    end
+
+    def destroy
+        session.clear
+        redirect_to login_path
+    end
+
+
 end
