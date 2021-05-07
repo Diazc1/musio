083e87a9eb321d839c47630ac8083e1c1ffd80e8
Cathy Diaz <Cathydiaz14@gmail.com>
2021-05-07 02:19:04 -0700

    create a scope method that will search for the name of songs in my app

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,8 +2,12 @@ class Song < ApplicationRecord
     has_many :comments 
     has_many :users, through: :comments
 
-    validates :name, presence: true
-    validates :artist_name, presence: true
-    validates :genre, presence: true
-    validates :lyrics, presence: true
+    validates :name, :artist_name, :genre, :lyrics, presence: true
+    validates :name, uniqueness: {scope: :name} 
+
+
+
+    scope :search, -> (query) { self.where("name LIKE ?", "%#{query}%") }
+
+
 end
