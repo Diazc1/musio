09fc0c4e862a4c8545809aef0d80c8f719db3886
Cathy Diaz <Cathydiaz14@gmail.com>
2021-05-07 02:02:36 -0700

    created fake data to add to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,287 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+# User.create([
+#     {username: "diazc", email: "diazc@gmail.com", password: "password"},
+#     {username: "diazcathy", email: "diazcathy@gmail.com", password: "password"},
+#     {username: "cathydiaz", email: "cathydiaz@gmail.com", password: "password"}
+# ])
+
+
+# Song.create([
+#     {name: "Te Deseo Lo Mejor", artist_name: "Bad Bunny", genre: "Urbano Latino", lyrics: "Yo sé que soy un ca**n
+#         Que no merezco tu perdón
+#         Te juro que no e' mi intención
+#         Pero si escucha' esta canción
+#         Te deseo lo mejor
+#         Ojalá que te olvides de mí
+#         Yo sé que fui lo peor
+#         Y tú mereces ser feli'
+#         Me voy antes de que sea tarde
+#         Y el corazón te destroce otra ve'
+#         Ya sé que soy un cobarde
+#         Si quiere' haz que no me conoce', está bien
+#         Otra ve' no cumplí lo que te prometí
+#         Otra ve' te fallé, otra ve' te mentí, eh-eh
+#         Otra ve' me toca ser el malo
+#         ¿De qué valen la' flore' y lo' regalo'?
+#         Si soy culpable de tu dolor
+#         Y por mí ahora ere' peor
+#         No quiere' que más nadie te hable de amor
+#         Pero yo
+#         Te deseo lo mejor
+#         Ojalá que te olvides de mí
+#         Yo sé que fui lo peor
+#         Y tú mereces ser feli'
+#         Me voy ante' de que sea tarde
+#         Y el corazón te destroce otra ve'
+#         Ya sé que soy un cobarde
+#         Si quiere' haz que no me conoce', está bien, eh"},
+#     {name: "Pray For You", artist_name: "Jhene Aiko", genre: "R&B/Soul", lyrics: "The feeling's finally mutual
+#         I'm ready to let you know
+#         Don't wanna see you no more
+#         I'm ready to let this go
+#         But never will I ever not wish you well
+#         Though we're not together, God bless you still
+#         It's gonna get better, I know it will
+#         Just hope you know, I still
+#         I'll pray for you, I'll pray for you
+#         I'll pray for you, I'll pray for you
+#         I pray you find your confidence
+#         Pray you find a confidant
+#         Pray you get everything that you want
+#         Pray you get everything that I could not give to you
+#         I know that it may seem weird to you, but
+#         Never will I ever not wish you well
+#         Though we're not together, God bless you still
+#         It's gonna get better, I know it will
+#         Just hope you know, I still
+#         I'll pray for you, I'll pray for you
+#         I'll pray for you, I'll pray for you
+#         I'll pray
+#         "},
+#     {name: "10K Hours", artist_name: "Jhene Aiko", genre: "R&B/Soul", lyrics: "Yeah
+#         One, two, check
+#         Check, one, two
+#         I know what it's like to lose, do you? Do you?
+#         Have you ever loved someone, then lost that one?
+#         You can't even call they phone
+#         You can't even ask how they day was
+#         Nothing in life's guaranteed, you'll see, uh
+#         I'm not being difficult, I just see things differently
+#         When I say I wanna see you, that means something deeper to me
+#         I've been missing you for ten thousand hours (thousand hours)
+#         I cannot let go, ten thousand memories (memories)
+#         I've been missing you for ten thousand hours, yeah (thousand hours)
+#         I cannot let go, ten thousand memories (memories)
+#         Uh, have you ever loved someone, then lost that one?
+#         Have you ever missed someone that ain't coming back? Uh
+#         You can't even tell that one 'cause they're not here to hear it
+#         Do you hear me?
+#         Nothing in life's guaranteed, you'll see
+#         I'm not being difficult, I just see things differently
+#         You don't see the urgency, that really worries me
+#         The problem is that we think, naturally, we have time but you'll find
+#         Anything can happen, don't take it for granted
+#         You know
+#         I've been missing you for ten thousand hours (ten thousand hours)
+#         I cannot let go, ten thousand memories (ten thousand memories)
+#         I've been missing you for ten thousand hours (ten thousand hours)
+#         Uh, I cannot let go (no), ten thousand memories (ten thousand memories)
+#         Yeah, thinking back on some good times, we have with some bad people like
+#         I shoulda saw she was see through or he was evil like
+#         I wasted my good experience with these people like
+#         Guess it was meant to be, you was teaching me life
+#         So only half that memory's worthwhile
+#         We had beef but at least there's a birth of a child
+#         Cooled and flipped it, reminiscing bad times with good people's terrific
+#         Beautifully different 'cause we still kick it
+#         You said we can't be intimate, you said we can't go there
+#         If this thing is going nowhere, it's so weird
+#         Now it's cold, thought I had control, thought I took your soul
+#         Nah, I took your mind and grave by name
+#         You told me that my mind is your favorite place to hang
+#         Ten thousand hours turned to ten thousand bridal flowers
+#         What was mine is ours
+#         How many soulmates we get in this lifetime?
+#         Right now's the right time, you the wife kind
+#         You could search your whole life tryna find
+#         A love as good as the time we had, we had (we had)
+#         Every person that comes in your life
+#         Here for all the muddy times
+#         So be fair, be there (be there)m uh
+#         Always planning, never manifesting
+#         Father, please help me send him this message (this message)
+#         Always planning, never manifesting
+#         Father, please help me send him this message"}
+# ])
+
+
+# Song.create([
+#     {name: "La Noche de Anoche", artist_name: "Bad Bunny and Rosalia", genre: "Urbano Latino", lyrics: "Yo sé que esto no volverá a pasar
+#         Pero si volviera a pasar
+#         Sé que sería tu debilidad
+#         Porque la noche de anoche fue (-ue)
+#         Algo que yo no puedo explicar
+#         Sola, dando y dándole sin parar (-rar)
+#         Tú me decías que morías por mí
+#         Porque la noche de anoche fue (eh eh)
+#         Algo que yo no puedo explicar (eh eh)
+#         Eso era dando y dándole sin parar
+#         Tú encima de mí, yo encima de ti (oh oh)
+#         Eh, eh
+#         Tú me dejaste el cuerpo caliente, infierno
+#         Pero me dejaste el corazón frío, invierno
+#         Soñando que contigo es que duermo (duermo)
+#         Dime, papi
+#         Dime, mami
+#         ¿Esa noche quién la borra?
+#         Tú me besaste y se me cayó la gorra
+#         Sin mucha labia, sin mucha cotorra
+#         Cuando estoy contigo dejo que la vibra corra
+#         Y que la luna nos supervise
+#         Con esa boquita suena rico todo lo que tú me dices
+#         Hicimos poses que yo más nunca hice
+#         Tú te mojaste pa que yo me bautice
+#         Y me ponga serio, serio
+#         Tú y yo juntos creando un imperio
+#         Esos ojitos tienen un misterio
+#         Pero al final nada de lo nuestro fue en serio
+#         Y ya me ha pasao que me han ilusionao
+#         Y ya me ha pasao que me han abandonao
+#         Y ya me ha pasao que no estás a mi lao
+#         Y ya me ha pasao
+#         Porque la noche de anoche fue (eh eh)
+#         Algo que yo no puedo explicar (eh eh)
+#         Eso era dando y dándole sin parar (eh eh)
+#         Tú encima de mí, yo encima de ti (oh oh)
+#         Porque la noche de anoche fue (eh eh)
+#         Algo que yo no puedo explicar (eh eh)
+#         Sola, dando y dándole sin parar (eh eh)
+#         Yo encima de ti, tú encima de mí (oh oh)
+#         Que yo me iría otra vez pa Japón (hey)
+#         Papi, qué penita tú, qué maldición (mmm)
+#         La paleta es dulce, azúcar de algodón (yeh)
+#         Y es la única que me llega hasta al corazón
+#         Yo no me olvido, la suerte está echada (bum)
+#         Se me enredaba el pelo en la pantalla
+#         Sabes que solo lo hago de leyenda (¡wuh!)
+#         Lo subo al cielo, yo soy su messiah
+#         El benito es un diablillo, ella es un ángel
+#         Lo tengo juqueao como si fuera tote
+#         Baby, entre nosotros nunca competimos
+#         Si preguntan dice, ella todos los récords bate
+#         Y ya me ha pasao que me han ilusionao
+#         Y ya me ha pasao que me han abandonao
+#         Y ya me ha pasao que no estás a mi lao
+#         Y ya me ha pasao
+#         Porque la noche de anoche fue (eh eh)
+#         Algo que yo no puedo explicar (eh eh)
+#         Eso era dando y dándole sin parar (eh eh)
+#         Tú encima de mí, yo encima de ti (oh oh)
+#         Porque la noche de anoche fue (eh eh)
+#         Algo que yo no puedo explicar (eh eh)
+#         Eso era dando, dándole sin parar (eh eh)
+#         Tú encima de mí, yo encima de ti (oh oh)
+#         Mmm jum
+#         Mmm
+#         Mmm, jum"},
+#     {name: "A Thousand Years", artist_name: "Christina Perri", genre: "POP", lyrics: "Heart beats fast
+#         Colors and promises
+#         How to be brave?
+#         How can I love when I'm afraid to fall?
+#         But watching you stand alone
+#         All of my doubt suddenly goes away somehow
+#         One step closer
+#         I have died every day waiting for you
+#         Darling, don't be afraid
+#         I have loved you for a thousand years
+#         I'll love you for a thousand more
+#         Time stands still
+#         Beauty in all she is
+#         I will be brave
+#         I will not let anything take away
+#         What's standing in front of me
+#         Every breath
+#         Every hour has come to this
+#         One step closer
+#         I have died every day waiting for you
+#         Darling, don't be afraid
+#         I have loved you for a thousand years
+#         I'll love you for a thousand more
+#         And all along I believed I would find you
+#         Time has brought your heart to me
+#         I have loved you for a thousand years
+#         I'll love you for a thousand more
+#         One step closer
+#         One step closer
+#         I have died every day waiting for you
+#         Darling don't be afraid
+#         I have loved you for a thousand years
+#         I'll love you for a thousand more
+#         And all along I believed I would find you
+#         Time has brought your heart to me
+#         I have loved you for a thousand years
+#         I'll love you for a thousand more
+#         "},
+#     {name: "Eyes on You", artist_name: "Chase Rice", genre: "Country", lyrics: "We've been to both Carolinas
+#         Seen a big Montana sky
+#         Driven up and down the coast a few times
+#         London, Paris, paradise
+#         But ain't got much memory of the scenes
+#         Come to think, I miss them all
+#         And damn, babe, it's all your fault
+#         No matter where we go
+#         No matter what we do
+#         If you're there, girl, I've got my eyes on you
+#         Don't matter where we've been
+#         No, there ain't no better view
+#         Than you in my arms with my eyes on you
+#         With my eyes on you
+#         (With my eyes on you)
+#         With my eyes on you
+#         (With my eyes on you)
+#         Speakin' of the coast, remember Pfeiffer Beach?
+#         You and me, that sunset, cliffs by the sea
+#         And the night rolled in
+#         And you still talk about that moon that I can't recall
+#         But God I saw
+#         All I needed to
+#         Might as well have stayed in the room
+#         No matter where we go
+#         No matter what we do
+#         If you're there, girl, I've got my eyes on you
+#         Don't matter where we've been
+#         No, there ain't no better view
+#         Than you in my arms with my eyes on you
+#         With my eyes on you
+#         (With my eyes on you)
+#         Yeah, with my eyes on you
+#         (With my eyes on you)
+#         And don't you remember Times Square?
+#         And people countin' down everywhere
+#         Two thousand whatever, no, I didn't care
+#         'Cause, baby, you were there
+#         And once again
+#         Couldn't help but stare
+#         No matter where we go
+#         No matter what we do
+#         If you're there, girl, I've got my eyes on you
+#         Don't matter where we've been
+#         No, there ain't no better view
+#         Than you in my arms with my eyes on you
+#         Halfway around the world
+#         Or the local corner booth
+#         Yeah, you guessed it, girl
+#         You know I got my eyes on you
+#         We could be on any street
+#         Any party, any room
+#         I'll be there all night with my eyes on you
+#         Don't matter where we've been
+#         No, there ain't no better view
+#         Than you in my arms with my eyes on you
+#         "}
+# ])
