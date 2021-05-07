a010cd87f531e30677eaa9957dcd5042a392bebc
Cathy Diaz <Cathydiaz14@gmail.com>
2021-05-07 02:10:02 -0700

    create helper methods to make code more DRY

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,18 @@
 module CommentsHelper
+
+    def fields_for_helper(f)
+        if @song 
+            f.hidden_field :song_id, value: @song.id
+        else 
+            render partial: "fields_for", locals: {f: f}
+        end
+    end
+
+    def index_header
+        if @song 
+            content_tag(:h1, "#{@song.name}'s comments")
+        else 
+            content_tag(:h1, "All Comments")
+        end 
+    end
 end
