dc33b6ebe605d4547ed3d30b4c6317ba7297d2de
Cathy Diaz <Cathydiaz14@gmail.com>
2021-05-07 02:26:31 -0700

    create CRUD actions. Create strong params and implement them throughout the code

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,59 @@
 class SongsController < ApplicationController
+
+    def search
+        @songs = Song.search(params[:name])
+        render :index
+    end
+
+    def index
+        @songs = Song.all
+    end
+
+    def show
+        @song = Song.find_by_id(params[:id])
+        @comment = Comment.new
+    end
+
+    def new
+        @song = Song.new
+    end
+
+    def create
+        @song = Song.create(song_params)
+
+        if @song.save 
+            redirect_to song_path(@song)
+        else
+            render :new
+        end
+    end
+
+
+    def edit
+        @song = Song.find_by_id(params[:id])
+    end
+
+    def update 
+        @song = Song.find_by_id(params[:id])
+        @song.update(song_params)
+
+        if @song.valid?
+            redirect_to song_path(@song)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        Song.find_by_id(params[:id]).destroy
+        redirect_to songs_path
+    end
+
+    private
+
+    def song_params
+        params.require(:song).permit(:name, :artist_name, :genre, :lyrics)
+    end
+
+
 end
